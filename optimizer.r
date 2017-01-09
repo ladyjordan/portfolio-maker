a398e47ffe61c2e1cfdccf119e3125b24b9b1339
Ankita Ravalji <ankitaravalji@gmail.com>
2017-01-10 02:12:07 +0530

    minor changes

--- optimizer.r
+++ optimizer.r
@@ -1,22 +1,22 @@
-# Portfolio Optimizer
+# Tech Portfolio Optimizer
 A <- read.csv("datasets/appl.csv")
 B <- read.csv("datasets/fb.csv")
 C <- read.csv("datasets/nvda.csv")
 D <- read.csv("datasets/googl.csv")
 
-# Historical stock prices of NASDAQ	listed comapanies(weekly frequency)
+# Historical stock prices of NASDAQ	listed - Apple Inc., Facebook Inc., Nvidia and Alphabet Inc. (weekly frequency)
 p <- structure(c(A$Close, B$Close, C$Close, D$Close), .Dim = c(52, 4), .Dimnames = list(NULL, c("APPLE", "FACEBOOK", "NVIDIA", "GOOGLE")))
 # Difference of prices to returns 
 r <- 100*diff(log(p))
-# Historical expected returns
+# expected returns
 colMeans(r)
-# Historical correlation matrix 
+# correlation matrix 
 cor(r)
-# Historical covariance matrix 
+# covariance matrix 
 S <- cov(r)
 S
 
-# Historical portfolio variance for stated portfolio of 20%,20%,30%,30%
+# portfolio variance for expected portfolio of 20%,20%,30%,30%
 w <- c(.2, .2, .3, .3)
 t(w) %*% S %*% w
 
@@ -25,7 +25,7 @@ library(tseries)
 optimised <- portfolio.optim(r)       
 # weights 
 optimised$pw 
-# Expected return using weights                          
+# expected return using weights                          
 optimised$pm    
- # Standard deviation of optimised portfolio                        
+ # standard deviation of optimised portfolio                        
 optimised$ps                           
